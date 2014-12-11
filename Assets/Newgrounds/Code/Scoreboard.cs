a4e85ea9a9439e71ca7ac983e6db987d4faf00fa
AdamRVierra <adam@adamvierra.com>
2014-12-11 12:26:07 -0800

    Test
    test

---
+++ Assets/Newgrounds/Code/Scoreboard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Newgrounds
+{
+	public class Scoreboard
+	{
+		public int m_id;
+		public string m_name;
+
+		public Scoreboard(JSONCollection collection) 
+		{
+			m_id = collection.FindInt("id");
+			m_name = collection.Find("name");
+		}
+	}
+}
